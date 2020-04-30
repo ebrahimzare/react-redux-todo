5040a917ccdb8636aa29161090e4c7c968bed7d5
Ebi <zare.math@gmail.com>
2020-04-29 23:07:05 -0400

    changed project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import App from './components/App'
-import rootReducer from './reducers'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import reducer from './reducers'
+import App from './containers/App'
 
-const store = createStore(rootReducer)
+const middleware = [ thunk ]
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
 
 render(
   <Provider store={store}>
